d2c489c54045f711d041b1efbe3ba071c726a1d0
Tim Disney <tim.disney@gmail.com>
2016-07-17 13:49:29 -0700

    wip

---
+++ src/reduce.js
@@ -0,0 +1,12 @@
+import Term from './term-spec';
+
+function transform(reducer, term) {
+  // if (List.isList(term)) {
+  //
+  // } else if ()
+  // Term.spec[term.type]
+}
+
+export default function reduce(reducer, term) {
+  return transform(reducer, term);
+}
