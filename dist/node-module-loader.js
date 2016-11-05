b9307f107b1bc469dfd76be1b9712603b1afc06c
Alex Lopatin <alex@siftscience.com>
2016-11-05 09:08:58 -0500

    Updated dist

--- dist/node-module-loader.js
+++ dist/node-module-loader.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = moduleLoader;
+// import { readFileSync } from 'fs';
 
-var _fs = require('fs');
-
-function moduleLoader(path) {
-  try {
-    return (0, _fs.readFileSync)(path, 'utf8');
-  } catch (e) {
-    return "";
-  }
+function moduleLoader() {
+  return "";
+  //   try {
+  //     return readFileSync(path, 'utf8');
+  //   } catch (e) {
+  //     return "";
+  //   }
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9ub2RlLW1vZHVsZS1sb2FkZXIuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7a0JBRXdCLFk7O0FBRnhCOztBQUVlLFNBQVMsWUFBVCxDQUFzQixJQUF0QixFQUE0QjtBQUN6QyxNQUFJO0FBQ0YsV0FBTyxzQkFBYSxJQUFiLEVBQW1CLE1BQW5CLENBQVA7QUFDRCxHQUZELENBRUUsT0FBTyxDQUFQLEVBQVU7QUFDVixXQUFPLEVBQVA7QUFDRDtBQUNGIiwiZmlsZSI6Im5vZGUtbW9kdWxlLWxvYWRlci5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IHJlYWRGaWxlU3luYyB9IGZyb20gJ2ZzJztcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gbW9kdWxlTG9hZGVyKHBhdGgpIHtcbiAgdHJ5IHtcbiAgICByZXR1cm4gcmVhZEZpbGVTeW5jKHBhdGgsICd1dGY4Jyk7XG4gIH0gY2F0Y2ggKGUpIHtcbiAgICByZXR1cm4gXCJcIjtcbiAgfVxufVxuIl19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9ub2RlLW1vZHVsZS1sb2FkZXIuanMiXSwibmFtZXMiOlsibW9kdWxlTG9hZGVyIl0sIm1hcHBpbmdzIjoiOzs7OztrQkFFd0JBLFk7QUFGeEI7O0FBRWUsU0FBU0EsWUFBVCxHQUF3QjtBQUNyQyxTQUFPLEVBQVA7QUFDRjtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0MiLCJmaWxlIjoibm9kZS1tb2R1bGUtbG9hZGVyLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IHsgcmVhZEZpbGVTeW5jIH0gZnJvbSAnZnMnO1xuXG5leHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBtb2R1bGVMb2FkZXIoKSB7XG4gIHJldHVybiBcIlwiO1xuLy8gICB0cnkge1xuLy8gICAgIHJldHVybiByZWFkRmlsZVN5bmMocGF0aCwgJ3V0ZjgnKTtcbi8vICAgfSBjYXRjaCAoZSkge1xuLy8gICAgIHJldHVybiBcIlwiO1xuLy8gICB9XG59XG4iXX0=
